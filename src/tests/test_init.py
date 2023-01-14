f77d2b24f2b85499d7326787debd49d33ac54ffc
William Valentin <will@squareffect.com>
2023-01-14 13:16:57 -0800

    Refactor of test statements and finish tests.

--- src/tests/test_init.py
+++ src/tests/test_init.py
@@ -1,28 +1,55 @@
 import pytest
-from pytest import MonkeyPatch
 import os
-from src.utils.constants import LOG_PATH, LOG_LEVEL
+from unittest import mock
+from src.utils.constants import LOG_LEVEL
 import src.utils.init as init
+import string
+import random
+
+rs = ''.join(random.choices(string.ascii_uppercase +
+                            string.digits, k=7))
 
 
-def test_log_path(monkeypatch: MonkeyPatch):
-    assert os.path.exists(LOG_PATH)
-    assert os.path.isdir(LOG_PATH)
-    assert os.path.exists(f"{LOG_PATH}/app.log")
-    assert os.path.exists(f"{LOG_PATH}/app.warning.log")
-    assert os.path.exists(f"{LOG_PATH}/app.error.log")
-
-    # monkeypatch.setenv('LOG_PATH', '')
-    # with pytest.raises(ValueError):
-    #     init.logger
+@mock.patch.dict(os.environ, {"LOG_PATH": "logs"})
+def test_log_path_valid() -> None:
+    log_path = os.getenv("LOG_PATH")
+    assert init.log_path(str(log_path)) == "logs"
 
 
-# @pytest.mark.run('last')
-def test_testing_mode() -> None:
+@mock.patch.dict(os.environ, {"LOG_PATH": ""})
+def test_log_path_invalid() -> None:
+    with pytest.raises(ValueError):
+        log_path = os.getenv("LOG_PATH")
+        init.log_path(str(log_path))
+
+
+def test_need_create_log_path():
+    assert init.create_log_path(f"/tmp/{rs}") is True
+
+
+def test_no_need_create_log_path():
+    assert init.create_log_path(f"/tmp/{rs}") is False
+
+
+@mock.patch.dict(os.environ, {"LOG_LEVEL": "DEBUG"})
+def test_log_level_valid() -> None:
     assert LOG_LEVEL in ["DEBUG", "INFO", "WARNING", "ERROR"]
-    if LOG_LEVEL == "DEBUG":
-        assert init.testing_mode is True
-    if LOG_LEVEL in ["INFO", "WARNING", "ERROR"]:
-        assert init.testing_mode is False
-    # else:
-    #     raise ValueError("LOG_LEVEL must be DEBUG, INFO, WARNING, or ERROR")
+
+
+@mock.patch.dict(os.environ, {"LOG_LEVEL": "TEST"})
+def test_log_level_invalid() -> None:
+    with pytest.raises(ValueError):
+        log_level = os.getenv("LOG_LEVEL")
+        init.testing_mode(str(log_level))
+
+
+@mock.patch.dict(os.environ, {"LOG_LEVEL": "DEBUG"})
+def test_testing_mode_true() -> None:
+    log_level = os.getenv("LOG_LEVEL")
+    assert init.testing_mode(str(log_level))
+
+
+@mock.patch.dict(os.environ, {"LOG_LEVEL": "INFO"})
+def test_testing_mode_false() -> None:
+    log_level = os.getenv("LOG_LEVEL")
+    assert not init.testing_mode(str(log_level))
